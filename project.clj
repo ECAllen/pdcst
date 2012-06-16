d9430a918cd1cb79e1ab47294b1535818fe4a0c1
Ethan Allen <ethancallen@gmail.com>
2012-06-16 00:21:44 -0400

    partial reorg and started cleanup

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.zip "0.1.0"]
-                 [clj-http "0.4.1"]])
+                 [http.async.client "0.4.5"]])
--- src/pdcst/core.clj
+++ src/pdcst/core.clj
@@ -5,45 +5,53 @@
       [clojure.data.zip :as zf]
       [clojure.data.zip.xml :as zxml]
       [clojure.string :as string]
-      [clj-http.client :as client]))
+      [clojure.java.io :as io]))
 
 (import '(java.io File))
 
+(def subscriptions "/home/ethan/.pdcst/pdcst.conf")
 
-(def subscriptions "/home/ethan/projects/pdcst/src/pdcst/subs.conf")
-
-(defn get-urls []
+(defn get-urls [subscriptions]
   (string/split-lines (slurp subscriptions)))
 
 ; (def url "http://reason.tv/podcast/index.xml")
-(def url "index.xml") 
 
-(def x (xml/parse url))
+;(defn get-mp3s [subscriptions]
+;  (map something (get-urls subscriptions)))
+
+;(defn something [])
+
+;(def pdcst-info (map get-pdcst-info (get-items x)))
+
 
 (def z (zip/xml-zip (xml/parse url)))
 
-(defn get-title [x] 
-  (let [title (first (zxml/xml-> (zip/xml-zip x) :channel :title zxml/text))]
-    (clojure.string/replace title #" |\." "-")))
+(def x (xml/parse url))
 
-(defn get-items [z]
-  (for [x (xml-seq z) 
-        :when (= :item (:tag x))]
+(defn get-items [url]
+  (for [x (xml-seq (xml/parse url) 
+        :when (= :item (:tag x)))]
     (:content x)))
 
 (defn get-pdcst-info [item]
   (let [[{title :tag [title-txt] :content} 
        {enclosure :tag {url :url} :attrs } 
        {desc :tag [desc-txt] :content}] item ] 
-          into {title title-txt enclosure url desc desc-txt}))
+          into {title title-txt enclosure url desc desc-txt "download?" false}))
 
-(def pdcst-info (map get-pdcst-info (get-items url)))
+; (def pdcst-info (map get-pdcst-info (get-items x)))
 
 ; file structure 
 ; ~/.pdcst
 ; ~/.pdcst/pdcst.conf
 ; ~/.pdcst/downloads
 ; ~/.pdcst/downloads/<pdcst name>
+
+(def x (xml/parse url))
+
+(defn get-title [x] 
+  (let [title (first (zxml/xml-> (zip/xml-zip x) :channel :title zxml/text))]
+    (clojure.string/replace title #" |\." "-")))
 
 (defn create-dir [dirname]
   (let [f (File. dirname)]
@@ -74,5 +82,11 @@
                              (create-download-dir))
       :else "ok")))
 
+;    (.write w (client/get "http://cloudfront-reasontv-video.reason.com/reasontv_audio_2520.mp3"))))
+
+(defn download [filename url]
+  (with-open [rdr (io/reader url)
+              wrt (io/writer filename)]
+    (io/copy rdr wrt)))
 
 
