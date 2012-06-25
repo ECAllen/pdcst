d0b149d6b57ac3db3f748f24bab3b37ef50db21a
Ethan <ethancallen@gmail.com>
2012-06-25 13:44:20 -0400

    added funcs to: create download dir, create podcast dir based on title

--- src/pdcst/core.clj
+++ src/pdcst/core.clj
@@ -9,6 +9,12 @@
 
 (import '(java.io File))
 
+; file structure 
+; ~/.pdcst
+; ~/.pdcst/pdcst.conf
+; ~/.pdcst/downloads
+; ~/.pdcst/downloads/<pdcst name>
+
 (def subscriptions "/home/ethan/.pdcst/pdcst.conf")
 
 (defn get-urls [subscriptions]
@@ -16,21 +22,15 @@
 
 ; (def url "http://reason.tv/podcast/index.xml")
 
-;(defn get-mp3s [subscriptions]
-;  (map something (get-urls subscriptions)))
+; (def pdcst-info (map get-pdcst-info (get-items x)))
 
-;(defn something [])
+; (def z (zip/xml-zip (xml/parse url)))
 
-;(def pdcst-info (map get-pdcst-info (get-items x)))
-
-
-(def z (zip/xml-zip (xml/parse url)))
-
-(def x (xml/parse url))
+; (def x (xml/parse url))
 
 (defn get-items [url]
-  (for [x (xml-seq (xml/parse url) 
-        :when (= :item (:tag x)))]
+  (for [x (xml-seq (xml/parse url))
+        :when (= :item (:tag x))]
     (:content x)))
 
 (defn get-pdcst-info [item]
@@ -41,16 +41,8 @@
 
 ; (def pdcst-info (map get-pdcst-info (get-items x)))
 
-; file structure 
-; ~/.pdcst
-; ~/.pdcst/pdcst.conf
-; ~/.pdcst/downloads
-; ~/.pdcst/downloads/<pdcst name>
-
-(def x (xml/parse url))
-
-(defn get-title [x] 
-  (let [title (first (zxml/xml-> (zip/xml-zip x) :channel :title zxml/text))]
+(defn get-title [url] 
+  (let [title (first (zxml/xml-> (zip/xml-zip (xml/parse url)) :channel :title zxml/text))]
     (clojure.string/replace title #" |\." "-")))
 
 (defn create-dir [dirname]
@@ -64,6 +56,10 @@
 (defn create-download-dir [] 
   (let [conf-dir (str (System/getProperty "user.home") "/.pdcst/download")]
     (create-dir conf-dir)))
+
+(defn create-pdcst-dir [title]
+  (let [d (str (System/getProperty "user.home") "/.pdcst/download/" title)]
+    (create-dir d)))
 
 (defn kind [filename]
   (let [f (File. filename)]
@@ -89,4 +85,6 @@
               wrt (io/writer filename)]
     (io/copy rdr wrt)))
 
+; TODO replace maps with ->
+; (map create-pdcst-dir (map get-title (get-urls subscriptions)))
 
